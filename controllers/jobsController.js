1b4176f5d4158ace425084c5510473516693c2d7
Hassan Bulega <hassanbulega@gmail.com>
2022-10-17 10:48:55 +0300

    feat: add job model and controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,11 +1,21 @@
+import Job from "../models/Job.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, NotFoundError } from "../errors/index.js";
+
 const createJob = async (req, res) => {
-  res.send("create Job ");
+  const { position, company } = req.body;
+  if (!position || !company) {
+    throw new BadRequestError("Please provide all values");
+  }
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 const getAllJobs = async (req, res) => {
   res.send("get all Jobs");
 };
 const updateJob = async (req, res) => {
-    res.send("update Job");
+  res.send("update Job");
 };
 const deleteJob = async (req, res) => {
   res.send("delete Job");
