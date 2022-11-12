d9060dca5b9c011fd955b063e1743b3903888899
Hassan Bulega <hassanbulega@gmail.com>
2022-11-12 10:24:52 +0300

    add context for job stats

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -24,6 +24,8 @@ import {
   EDIT_JOB_BEGIN,
   EDIT_JOB_SUCCESS,
   EDIT_JOB_ERROR,
+  SHOW_STATS_BEGIN,
+  SHOW_STATS_SUCCESS,
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -52,6 +54,8 @@ const initialState = {
   totalJobs: 0,
   numOfPages: 1,
   page: 1,
+  stats:{},
+  monthlyApplications:[],
 };
 
 const AppContext = React.createContext();
@@ -246,6 +250,23 @@ const AppProvider = ({ children }) => {
       logoutUser();
     }
   };
+  const showStats = async ()=> {
+    dispatch({type:SHOW_STATS_BEGIN})
+    try {
+      const {data}= await authFetch('/jobs/stats')
+      dispatch({
+        type:SHOW_STATS_SUCCESS,
+        payload: {
+          stats:data.defaultStats,
+          monthlyApplications:data.monthlyApplications,
+        }
+      })
+      clearAlert()
+    } catch (error) {
+      console.log(error.response);
+      logoutUser()
+    }
+  }
 
   return (
     <AppContext.Provider
@@ -264,6 +285,7 @@ const AppProvider = ({ children }) => {
         setEditJob,
         deleteJob,
         editJob,
+        showStats,
       }}
     >
       {children}
