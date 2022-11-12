1e6d5b2cbfc857e207eb31d7c40368687ca07427
Hassan Bulega <hassanbulega@gmail.com>
2022-11-12 10:25:58 +0300

    add stats for jobs and fix path in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
             </ProtectedRoute>
           }
         >
-          <Route index path='stats' element={<Stats />} />
+          <Route index path='/' element={<Stats />} />
           <Route path='all-jobs' element={<AllJobs />} />
           <Route path='add-job' element={<AddJob />} />
           <Route path='profile' element={<Profile />} />
